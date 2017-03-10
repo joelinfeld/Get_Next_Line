c0c759c4735bf0232991128b45cf579d63d3be1e
Joel INFELD <jinfeld@e1z2r2p11.42.us.org>
2017-03-09 19:57:49 -0800

    git next

--- get_next_line.c
+++ get_next_line.c
@@ -6,31 +6,47 @@
 /*   By: jinfeld <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/07 14:41:04 by jinfeld           #+#    #+#             */
-/*   Updated: 2017/03/07 20:36:43 by jinfeld          ###   ########.fr       */
+/*   Updated: 2017/03/09 13:49:21 by jinfeld          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int		linefill(char **line, char *save)
+{
+	char	end;
+
+	end = ft_strchr(save, '\n');
+	if (end != NULL)
+	{
+		end = '\0';
+	}
+}
+
 int		get_next_line(const int fd, char **line)
 {
 	static char *save;
-	char	buf[BUFF_SIZE + 1]
+	char		buf[BUFF_SIZE + 1]
+	char		*tmp;
+	int			ret;
 	
-	if(!(buf = (char*)ft_memalloc(BUFF_SIZE + 1)))
-		return (-1);
 	if (fd < 0)
 		return (-1);
+	if (save == NULL)
+		save = ft_strnew(0);
 	while (!ft_strchr(save, '\n'))
 	{
-		ret = read(fd, *line, BUFF_SIZE)
+		ret = read(fd, buf, BUFF_SIZE)
 		if (ret < 0)
 			return (-1);
 		if (ret == 0)
 			break ;
 		line[ret] = '\0';
-		str = ft_strjoin(str, buf);
+		tmp = ft_strjoin(save, buf);
+		ft_strdel(save);
+		save = tmp;
 	}
+	return(linefill(line, save));
 	if (close(fd) < 0)
 		return (-1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jinfeld <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/07 14:42:00 by jinfeld           #+#    #+#             */
-/*   Updated: 2017/03/07 14:45:57 by jinfeld          ###   ########.fr       */
+/*   Updated: 2017/03/09 14:05:20 by jinfeld          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,9 @@
 # define BUFF_SIZE 32
 # include <stdlib.h>
 
+struct			s_bufd
+{
+	int			fd;
+	char 		*buffer;
+}				t_bufd;
 int get_next_line(const int fd, char **line);
