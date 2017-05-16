c585dec073f36dfa8190737db4b82a3866d7d4bb
Joel INFELD <jinfeld@e1z2r7p48.42.us.org>
2017-05-16 16:25:16 -0700

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,77 +6,53 @@
 /*   By: jinfeld <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/07 14:41:04 by jinfeld           #+#    #+#             */
-/*   Updated: 2017/03/21 19:05:56 by jinfeld          ###   ########.fr       */
+/*   Updated: 2017/04/13 17:01:39 by jinfeld          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
-
-/*
-char	*ft_strjoinfree(char *s1, char *s2, int f)
-{
-	char	*s3;
-
-	if (s2 == NULL)
-		return (NULL);
-	if (s1 == NULL)
-		return (ft_strdup(s2));
-	s3 = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
-	if (s3)
-	{
-		ft_strcpy(s3, s1);
-		ft_strcat(s3, s2);
-	}
-	if (f == 1)
-		ft_strdel(&s1);
-	else if (f == 2)
-		ft_strdel(&s2);
-	return (s3);
-}
-*/
 
 int		linefill(char **line, char **save)
 {
 	char *end;
+	char *tmp;
 
-	if(ft_strlen(*save))
+	if (ft_strlen(*save))
 	{
 		end = ft_strchr(*save, '\n');
 		if (end != NULL)
 		{
-			*end = '\0';
+			*end++ = '\0';
 			*line = ft_strdup(*save);
-			ft_strclr(*save);
-			*save = ++end;
-			return(1);
+			tmp = ft_strdup(end);
+			ft_strdel(save);
+			*save = ft_strdup(tmp);
+			ft_strdel(&tmp);
+			return (1);
 		}
 		else
 		{
 			*line = ft_strdup(*save);
 			ft_strclr(*save);
-			return (1);	
+			return (1);
 		}
 	}
 	return (0);
 }
 
-t_list	*bufind(t_list **bufd, int fd)
+void	bufind(t_list **bufd, t_list **current, int fd)
 {
-	t_list	*current;
-		
-	current = *bufd;
-	while (current)
+	*current = *bufd;
+	while (*current)
 	{
-		if ((int)current->content_size == fd)
-			return(current);
-		current = current->next;
+		if ((int)(*current)->content_size == fd)
+			return ;
+		*current = (*current)->next;
 	}
-	current = NULL;
 	ft_lstadd(bufd, ft_lstnew(NULL, 0));
 	(*bufd)->content_size = fd;
 	(*bufd)->content = ft_strnew(0);
-	return (*bufd);
+	*current = *bufd;
 }
 
 int		get_next_line(const int fd, char **line)
@@ -86,45 +62,23 @@ int		get_next_line(const int fd, char **line)
 	char			buf[BUFF_SIZE + 1];
 	char			*tmp;
 	int				ret;
-	
+
 	if (fd < 0 || !line || BUFF_SIZE < 0)
 		return (-1);
-	current = bufind(&bufd, fd);
+	bufind(&bufd, &current, fd);
+	*line = NULL;
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		if (ret < 0)
 			return (-1);
 		buf[ret] = '\0';
-		tmp = ft_strjoinfree(current->content, buf, 1);
+		tmp = ft_strjoin(current->content, buf);
 		ft_strdel((char**)&current->content);
 		current->content = ft_strdup(tmp);
 		ft_strdel(&tmp);
 		ft_strclr(buf);
-	
 		if (ft_strchr(current->content, '\n'))
 			break ;
 	}
-	return(linefill(line, (char**)&current->content));
+	return (linefill(line, (char**)&current->content));
 }
-
-/*
-int			main(int ac, char **av)
-{
-	int		fd;
-	char	*line;
-
-	if (ac == 1)
-		fd = 0;
-	if (ac == 2)
-		fd = open(av[1], O_RDONLY);
-	else return (2);
-	while	(get_next_line(fd, &line) == 1)
-	{
-		ft_putendl(line);
-		free(line);
-	}
-	if (ac == 2)
-		close (fd);
-	return(0);
-}
-*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,22 @@
 /*   By: jinfeld <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/07 14:42:00 by jinfeld           #+#    #+#             */
-/*   Updated: 2017/03/21 19:07:43 by jinfeld          ###   ########.fr       */
+/*   Updated: 2017/04/13 17:02:11 by jinfeld          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
-# include "libft.h"
+# define BUFF_SIZE 2
+# include "libft/libft.h"
+# include <fcntl.h>
 
-struct	s_bufd
+struct				s_bufd
 {
-	int					fd;
-	char 				*save;
-	struct s_bufd 		*next;
-}				t_bufd;
-int get_next_line(const int fd, char **line);
+	int				fd;
+	char			*save;
+	struct s_bufd	*next;
+}					t_bufd;
+int					get_next_line(const int fd, char **line);
 
 #endif
